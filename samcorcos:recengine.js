4906b41fd7560ec436dd239257b0fa2fc3f1b77c
Sam Corcos <samcorcos@gmail.com>
2015-01-27 18:07:25 -0500

    cleaning, though still broken

--- samcorcos:recengine.js
+++ samcorcos:recengine.js
@@ -103,19 +103,19 @@ var FlowNetwork = function() {
 recEngine = {}
 
 recEngine.link = function(user, item) {
-  var allItems = [];
-  var allEdges = RecEngine.find().fetch()
-  var allUsers = RecEngineLinks.find().fetch()
+  // var allItems = [];
+  // var allEdges = RecEngine.find().fetch()
+  // var allUsers = RecEngineLinks.find().fetch()
 
   // Add 1 to the weight if the items have been upvoted by similar people
   var incrementWeight = function() {
     var temp = RecEngineLinks.find({ user: user }).fetch();
     temp.forEach(function(link) {
       var tempArray;
-      if (link.item !== item) {
+      if (link.item !== item) { // Makes sure item isnt associated with itself
         tempArray = [link.item, item];
         tempArray.sort();
-        RecEngine.update({ nodes: tempArray }, { $inc: { weight: 1 }});
+        RecEngine.upsert({ nodes: tempArray }, { $inc: { weight: 1 }});
       }
     });
   };
@@ -130,30 +130,30 @@ recEngine.link = function(user, item) {
   };
   addUserPair();
 
-  // Get all items in an array
-  var getAllItems = function() {
-    allUsers.forEach(function(pair) {
-      if (allItems.indexOf(pair.item) === -1) {
-        allItems.push(pair.item);
-      }
-    });
-  };
-  getAllItems();
+  // // Get all items in an array
+  // var getAllItems = function() {
+  //   allUsers.forEach(function(pair) {
+  //     if (allItems.indexOf(pair.item) === -1) {
+  //       allItems.push(pair.item);
+  //     }
+  //   });
+  // };
+  // getAllItems();
 
-  // Do we need to set the default value?
-  var setDefaultValue = function() {
-    allItems.forEach(function(xitem) {
-      var tempArray;
-      if (xitem !== item) {
-        tempArray = [item, xitem];
-        tempArray.sort();
-        if (RecEngine.find({nodes: tempArray}).fetch().length === 0) {
-          RecEngine.insert({ nodes: tempArray, weight: 0 });
-        }
-      }
-    });
-  };
-  setDefaultValue();
+  // // Do we need to set the default value?
+  // var setDefaultValue = function() {
+  //   allItems.forEach(function(xitem) {
+  //     var tempArray;
+  //     if (xitem !== item) {
+  //       tempArray = [item, xitem];
+  //       tempArray.sort();
+  //       if (RecEngine.find({nodes: tempArray}).fetch().length === 0) {
+  //         RecEngine.insert({ nodes: tempArray, weight: 0 });
+  //       }
+  //     }
+  //   });
+  // };
+  // setDefaultValue();
 
   return "Successfully Linked"
 }
@@ -175,39 +175,44 @@ recEngine.suggest = function(userId, numSuggestions, cb) {
   };
   getAllItems();
 
-  RecEngine.find({ weight: { $lt: 9999999999 }}).fetch().forEach(function(x) {
-    allEdges.push(x);
-  }) // Finds all edges, minus user edges.
-  RecEngine.find({ nodes: { $in: [userId] }}).fetch().forEach(function(x) {
-    allEdges.push(x);
-  }) // Finds all edges associated with the current user
+  if (RecEngine.find({nodes: { $in: [userId]}}).fetch().length > 0) { // Error handles if suggesting for user that does not exist
+    RecEngine.find({ weight: { $lt: 9999999999 }}).fetch().forEach(function(x) {
+      allEdges.push(x);
+    }) // Finds all edges, minus user edges.
+    RecEngine.find({ nodes: { $in: [userId] }}).fetch().forEach(function(x) {
+      allEdges.push(x);
+    }) // Finds all edges associated with the current user
 
-  allItems.forEach(function(item) {
-    var fn = new FlowNetwork(); // Must make a new flow network for each suggestion
+    allItems.forEach(function(item) {
+      var fn = new FlowNetwork(); // Must make a new flow network for each suggestion
 
-    allEdges.forEach(function(edge) { // add all the edges
-      fn.addEdge(edge.nodes[0], edge.nodes[1], edge.weight);
+      allEdges.forEach(function(edge) { // add all the edges
+        fn.addEdge(edge.nodes[0], edge.nodes[1], edge.weight);
+
+      })
+
+      if (fn.maxFlow(userId, item) < 9999999999) {
+        result.push({ suggestion: item, weight: fn.maxFlow(userId, item) })
+      }
+      // As long as the item isn't directly touching the user, add it to the results array
 
     })
 
-    if (fn.maxFlow(userId, item) < 9999999999) {
-      result.push({ suggestion: item, weight: fn.maxFlow(userId, item) })
+
+    // Sort the results in order of weight
+    result.sort(function(a,b) {
+      if (a.weight < b.weight) { return 1; }
+      if (a.weight > b.weight) { return -1; }
+      return 0;
+    })
+
+    // Error handle for insufficient data
+    if (numSuggestions > result.length) {
+      error = "Insufficient data! Only data for " + result.length + " suggestions."
+      console.error(error);
     }
-    // As long as the item isn't directly touching the user, add it to the results array
-
-  })
-
-
-  // Sort the results in order of weight
-  result.sort(function(a,b) {
-    if (a.weight < b.weight) { return 1; }
-    if (a.weight > b.weight) { return -1; }
-    return 0;
-  })
-
-  // Error handle for insufficient data
-  if (numSuggestions > result.length) {
-    error = "Insufficient data! Only data for " + result.length + " suggestions."
+  } else {
+    error = "User does not exist!"
     console.error(error);
   }
 
